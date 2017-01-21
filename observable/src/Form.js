24c5d6dadc6578138a1414a92463c0dfc1c3f3d5
Grzegorz Szeliga <szeligagrzegorz@gmail.com>
2017-01-21 15:59:51 +0100

    pozbycie się ostatniej zależności od starego komponentu

--- observable/src/Form.js
+++ observable/src/Form.js
@@ -1,28 +1,11 @@
 /* @flow */
-import React from 'react';
+import React, { Component } from 'react';
 import Rx from 'rxjs';
 
-import BaseComponent3 from './_old/BaseComponent3';
+import { createRxComponent } from './Base';
 
-type MessageItem = {
-    id: string,
-    message: string
-};
-
-type StateType = {
-    message1: string | null,
-    message2: string | null,
-    message3: string | null,
-    messages: Array<MessageItem>,
-    sender1: (value: SyntheticEvent) => void,
-    sender2: (value: SyntheticEvent) => void,
-    sender3: (value: SyntheticEvent) => void,
-    submit: (value: SyntheticEvent) => void,
-};
 
 //https://github.com/acdlite/react-rx-component/blob/master/src/funcSubject.js
-
-//: rxjs$Subject<string | null>
 
 type SenderSubjectType<T> = {
     send: rxjs$Observable<(value: T) => void>,
@@ -35,7 +18,7 @@ function senderSubject<T>(): SenderSubjectType<T> {
     const sendValue = (value: T) => {
         subject.next(value);
     };
-    const behaviorSubject: rxjs$BehaviorSubject<(value: T) => void> = new Rx.BehaviorSubject(sendValue);
+    const behaviorSubject: rxjs$BehaviorSubject<(value: T) => void> = new Rx.BehaviorSubject(sendValue);      //TODO - Do wywalenia
 
     return {
         receive: subject.asObservable(),
@@ -43,132 +26,118 @@ function senderSubject<T>(): SenderSubjectType<T> {
     };
 }
 
-class Form extends BaseComponent3 {
 
-    props: {};
-    state: StateType;
-    _mount: bool;
+type MessageItem = {
+    id: string,
+    message: string
+};
 
-    constructor(props: {}) {
-        super(props);
+type PropsInType = {};
 
-        const sender1: SenderSubjectType<SyntheticEvent> = senderSubject();
-        const sender2: SenderSubjectType<SyntheticEvent> = senderSubject();
-        const sender3: SenderSubjectType<SyntheticEvent> = senderSubject();
-        const submit : SenderSubjectType<SyntheticEvent> = senderSubject();
+type PropsOutType = {
+    message1: string | null,
+    message2: string | null,
+    message3: string | null,
+    messages: Array<MessageItem>,
+    sender1: (value: SyntheticEvent) => void,
+    sender2: (value: SyntheticEvent) => void,
+    sender3: (value: SyntheticEvent) => void,
+    submit: (value: SyntheticEvent) => void,
+};
 
-        const message1$: rxjs$Observable<string | null> = sender1.receive
-            .map((e: SyntheticEvent): string => e.target instanceof HTMLInputElement ? e.target.value : '')
-            .startWith('')
-            .map(value => value.length > 3 ? 'Podaj maksymalnie 3 znaki' : null);
 
-        const message2$ = sender2.receive
-            .map((e: SyntheticEvent): string => e.target instanceof HTMLInputElement ? e.target.value : '')
-            .startWith('')
-            .map(value => value !== 'xx' ? 'Proszę wprowadzić frazę "xx"': null);
 
-        const message3$ = sender3.receive
-            .map((e: SyntheticEvent): string => e.target instanceof HTMLInputElement ? e.target.value : '')
-            .startWith('')
-            .map(value => value !== 'rr' ? 'Proszę wprowadzić frazę "rr"': null);
+const mapToProps = (props$: rxjs$Observable<PropsInType>): rxjs$Observable<PropsOutType> => {
 
-        const messages$ = Rx.Observable.combineLatest(message1$, message2$, message3$, (message1, message2, message3) => {
-            const messages = [];
+    const sender1: SenderSubjectType<SyntheticEvent> = senderSubject();
+    const sender2: SenderSubjectType<SyntheticEvent> = senderSubject();
+    const sender3: SenderSubjectType<SyntheticEvent> = senderSubject();
+    const submit : SenderSubjectType<SyntheticEvent> = senderSubject();
 
-            if (message1 !== null) {
-                messages.push({
-                    id: 'message1',
-                    message: message1
-                });
-            }
+    const message1$: rxjs$Observable<string | null> = sender1.receive
+        .map((e: SyntheticEvent): string => e.target instanceof HTMLInputElement ? e.target.value : '')
+        .startWith('')
+        .map(value => value.length > 3 ? 'Podaj maksymalnie 3 znaki' : null);
 
-            if (message2 !== null) {
-                messages.push({
-                    id: 'message2',
-                    message: message2
-                });
-            }
+    const message2$ = sender2.receive
+        .map((e: SyntheticEvent): string => e.target instanceof HTMLInputElement ? e.target.value : '')
+        .startWith('')
+        .map(value => value !== 'xx' ? 'Proszę wprowadzić frazę "xx"': null);
 
-            if (message3 !== null) {
-                messages.push({
-                    id: 'message3',
-                    message: message3
-                });
-            }
+    const message3$ = sender3.receive
+        .map((e: SyntheticEvent): string => e.target instanceof HTMLInputElement ? e.target.value : '')
+        .startWith('')
+        .map(value => value !== 'rr' ? 'Proszę wprowadzić frazę "rr"': null);
 
-            return messages;
-        }).distinctUntilChanged();
+    const messages$ = Rx.Observable.combineLatest(message1$, message2$, message3$, (message1, message2, message3) => {
+        const messages = [];
 
-        const combineResult = (
-            message1: string | null,
-            message2: string | null,
-            message3: string | null,
-            messages: Array<MessageItem>,
-            sender1: (value: SyntheticEvent) => void,
-            sender2: (value: SyntheticEvent) => void,
-            sender3: (value: SyntheticEvent) => void,
-            submit: (value: SyntheticEvent) => void
-        ): StateType => {
-            return {
-                message1: message1,
-                message2: message2,
-                message3: message3,
-                messages: messages,
-                sender1: sender1,
-                sender2: sender2,
-                sender3: sender3,
-                submit: submit
-            };
+        if (message1 !== null) {
+            messages.push({
+                id: 'message1',
+                message: message1
+            });
+        }
+
+        if (message2 !== null) {
+            messages.push({
+                id: 'message2',
+                message: message2
+            });
+        }
+
+        if (message3 !== null) {
+            messages.push({
+                id: 'message3',
+                message: message3
+            });
+        }
+
+        return messages;
+    }).distinctUntilChanged();
+
+    const combineResult = (
+        message1: string | null,
+        message2: string | null,
+        message3: string | null,
+        messages: Array<MessageItem>,
+        sender1: (value: SyntheticEvent) => void,
+        sender2: (value: SyntheticEvent) => void,
+        sender3: (value: SyntheticEvent) => void,
+        submit: (value: SyntheticEvent) => void
+    ): PropsOutType => {
+        return {
+            message1: message1,
+            message2: message2,
+            message3: message3,
+            messages: messages,
+            sender1: sender1,
+            sender2: sender2,
+            sender3: sender3,
+            submit: submit
         };
+    };
 
-        const state$: rxjs$Observable<StateType> = Rx.Observable.combineLatest(
-            message1$,
-            message2$,
-            message3$,
-            messages$,
-            sender1.send,
-            sender2.send,
-            sender3.send,
-            submit.send,
-            combineResult
-        );
+    return Rx.Observable.combineLatest(
+        message1$,
+        message2$,
+        message3$,
+        messages$,
+        sender1.send,
+        sender2.send,
+        sender3.send,
+        submit.send,
+        combineResult
+    );
+}
 
-        state$.subscribe((state: StateType) => {
-            if (this._mount === true) {
-                this.setState(state);
-            } else {
-                this.state = state;
-            }
-        });
 
-/*
-        this.onProps((propsStream: rxjs$Observable<PropsType>): rxjs$Subscription =>
-            state$.subscribe((state: StateType) => this.setState(state))
-        );
-*/
+class Form extends Component {
 
-/*
-this.onProps((propsStream: rxjs$Observable<PropsType>): rxjs$Subscription =>
-    propsStream
-        .map(props => props.id)
-        .distinctUntilChanged()
-        .switchMap(id => Store.getUser(id))
-        .subscribe((nextModel) => this.setState({ model: nextModel }))
-);
-*/
-        //.debounceTime(1000)
-    }
-
-    componentWillMount() {
-        this._mount = true;
-    }
-
-    componentWillUnmount() {
-        this._mount = false;
-    }
+    props: PropsOutType;
 
     render(): React.Element<*> {
-        const { message1, message2, message3, messages, sender1, sender2, sender3, submit} = this.state;
+        const { message1, message2, message3, messages, sender1, sender2, sender3, submit} = this.props;
 
         return (
             <div className="test_form">
@@ -211,4 +180,12 @@ this.onProps((propsStream: rxjs$Observable<PropsType>): rxjs$Subscription =>
     }
 }
 
-export default Form;
+const FormFn = (props: PropsOutType): React.Element<*> => {
+    return (
+        <Form {...props} />
+    );
+};
+
+const FormExport: (props: PropsInType) => React.Element<*> = createRxComponent(mapToProps, FormFn);
+
+export default FormExport;
