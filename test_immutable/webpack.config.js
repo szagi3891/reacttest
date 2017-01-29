032f4dcee1ee5f9bdacd72e12630431b4db765f3
Grzegorz Szeliga <szeligagrzegorz@gmail.com>
2017-01-29 09:48:53 +0100

    poprawienie konfiguracji

--- test_immutable/webpack.config.js
+++ test_immutable/webpack.config.js
@@ -21,7 +21,7 @@ module.exports = {
     },
     output: {
         path: path.join(__dirname, 'dist'),
-        publicPath: '/dist/',
+        publicPath: '/',
         filename: 'bundle.js'
     },
     plugins: [/*{
